dddb7ceb8526401f41cdde17dd7e8fe421320b80
JG <jeangerardhounkanrin@gmail.com>
2026-03-30 17:36:14 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+
+
 terraform {
   required_providers {
     render = {
@@ -12,17 +14,11 @@ provider "render" {
   owner_id = var.render_owner_id
 }
 
-# --- DÉCLARATION DES VARIABLES ---
-variable "render_api_key" { type = string }
-variable "render_owner_id" { type = string }
-variable "image_url" { type = string }
-variable "image_tag" { type = string }
 variable "github_actor" {
   description = "GitHub username"
   type        = string
 }
 
-# --- RESSOURCE ---
 resource "render_web_service" "flask_app" {
   name   = "flask-render-iac-${var.github_actor}"
   plan   = "free"
@@ -35,7 +31,6 @@ resource "render_web_service" "flask_app" {
     }
   }
 
-  # L'injection de la variable d'environnement (Exercice 2)
   env_vars = {
     ENV = {
       value = "production"
